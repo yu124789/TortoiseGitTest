417b019035554836040f3301d26fa27ebc6f6d1a
yu124789 <847771987@qq.com>
2017-11-27 15:17:38 +0800

    test..

---
+++ o_ o.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<time.h>
+
+int main(){
+    srand((unsigned)time(NULL));
+    while(1){
+        system("cls");
+        if (int i = rand()%5){
+            printf("o_ o \n\t");
+        }else{
+            printf("-_ -\n\t");
+        }
+        Sleep(200);
+    }
+    return 0;
+}
